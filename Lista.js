4e15eef7accb4efac008db5e7a668061748fcd35
Oscar Rodenas <rodenas.oscar@gmail.com>
2022-05-16 12:48:25 +0200

    arrays

---
+++ Lista.js
@@ -0,0 +1,14 @@
+const MyList =  [ 
+  "Óscar", 
+  25, 
+  true, 
+  new Date(1997, 09, 25), 
+  {
+    title: "JavaScript: The Definitive guide", 
+    author: "David Flanagan", 
+    Date: new Date(2007, 05, 04), 
+    url: "https://www.amazon.es/JavaScript-Definitive-Guide-Guides/dp/0596805527" 
+  }
+]
+
+console.log(MyList)
